8f21d37c137cf08ba48ea8d95fa0dc39c9f10593
ahmedalimalik99 <ahmedalimalik661@gmail.com>
2025-10-17 16:43:44 +0500

    final changes

--- eda.py
+++ eda.py
@@ -1,4 +1,4 @@
-# import libraries
+# Import libraries
 import pandas as pd
 import numpy as np
 import streamlit as st
@@ -6,44 +6,49 @@ import seaborn as sns
 from ydata_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
 
-# webapp tittle
-st.markdown('''
+# Web App Title
+st.markdown("""
 # **Exploratory Data Analysis Web Application**
 This app is developed by Ahmed Ali Malik
-''')
+""")
 
-# how to upload a file from pc
+# Sidebar - File Uploader
 with st.sidebar.header("Upload your dataset (.csv)"):
-    uploaded_file = st.sidebar.file_uploader("Upload your file", type=['csv'])
-    df = sns.load_dataset('titanic')
-    # st.sidebar.markdown("[Example CSV file](df)")
+    uploaded_file = st.sidebar.file_uploader("Upload your CSV file", type=['csv'])
+    st.sidebar.markdown("---")
+    st.sidebar.write("Or use example datasets below:")
 
-# profiling report for pandas
+# Load default dataset (Titanic)
+df = sns.load_dataset('titanic')
 
+# Function to load user-uploaded CSV
+@st.cache_data
+def load_csv(file):
+    csv = pd.read_csv(file)
+    return csv
+
+# Main Logic
 if uploaded_file is not None:
-    st.cache_data
-    def load_csv():
-        csv = pd.read_csv(uploaded_file)
-        return csv
-    df = load_csv()
+    df = load_csv(uploaded_file)
     pr = ProfileReport(df, explorative=True)
-    st.header('**Input DF**')
+    st.header("**Input DataFrame**")
     st.write(df)
-    st.write('---')
-    st.header('**Profiling report with Pandas**')
+    st.write("---")
+    st.header("**Pandas Profiling Report**")
     st_profile_report(pr)
+
 else:
-    st.info('Awaiting for CSV file, kindly upload file!')
-    if st.button('Press to use example data'):
-        # example dataset
-        st.cache_data
+    st.info("👆 Please upload a CSV file to start the analysis.")
+    if st.button("Or Press to Use Example Random Data"):
+        @st.cache_data
         def load_data():
-            a = pd.DataFrame( np.random.rand(100,5),
-                             columns=['A','B','C','D','E'])
+            a = pd.DataFrame(np.random.rand(100, 5), columns=['A', 'B', 'C', 'D', 'E'])
             return a
+
         df = load_data()
         pr = ProfileReport(df, explorative=True)
-        st.header('**Input DataFrame**')
-        st.write('---')
-        st.header('**Pandas Profiling Report**')
+        st.header("**Example Input DataFrame**")
+        st.write(df)
+        st.write("---")
+        st.header("**Pandas Profiling Report**")
         st_profile_report(pr)
